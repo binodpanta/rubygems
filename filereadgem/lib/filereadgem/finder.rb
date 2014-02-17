f5cbc220e2e6dd1f5e03e731eba864e1278938f5
Binod Pant <binod80@gmail.com>
2014-02-17 14:09:57 -0500

    Add ability to list all matching lines

--- filereadgem/lib/filereadgem/finder.rb
+++ filereadgem/lib/filereadgem/finder.rb
@@ -1,15 +1,19 @@
+# finds all lines matching the given str pattern in the file
+# basically a 'grep'
 class Finder
     def initialize(file,str)
         @str = str
         @file = file
     end
     def read
+        lines= []
         File.open(@file) { |f|
             f.each { |line|
                 line.chomp!;
-                return line if line.include? @str
+                lines << line if line.include? @str
             }
             
         }
+        lines
     end
 end
